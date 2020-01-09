4a4d01730e328958c23fceeacdbf2b7d4441e04e
Aodhagan <michael.m.malone@protonmail.com>
2020-01-09 20:28:33 +0000

    completed up to section Boolean statements

--- Chapter 5 code.R
+++ Chapter 5 code.R
@@ -1,4 +1,40 @@
+
+
+
+
 # changing values of an element in a vector
 vec <- c(0, 0, 0, 0, 0, 0)
 vec[1] <- 1000
 
+
+
+
+
+# exercise - extract the face column of deck2 and test
+# whether each value is equal to "ace"
+ace <- deck2[deck2$face == "ace"]
+ace
+# count how many cards  are equal to  "ace"
+sum(ace)
+
+# deck for hearts - every card has a value of zero
+deck4 <- deck
+deck4$value <- 0
+head(deck4, 13)
+
+# except cards in the suit of hearts and the Queen of spades
+# which is worth 13
+deck4$value[deck4$suit == "hearts"] <- 1
+deck4
+
+deck4$value[deck4$suit == "hearts" & deck4$face == "queen"] <- 13
+deck4
+
+
+# exercise - Boolean tests
+## is w positive
+w <- c(-1, 0, 1)
+w > 0
+## is x greater than 10 and less than 20
+x <- c(5, 15)
+x > 10 & x < 20
